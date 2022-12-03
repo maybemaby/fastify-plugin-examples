9391d90194a40c09a301da880fe69c2712423207
maybemaby <74226064+maybemaby@users.noreply.github.com>
2022-12-02 23:54:09 -0800

    init project

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dracula",
+      langs: ["ts"],
+    },
+  },
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+});
